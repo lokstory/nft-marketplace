e6e9e567f239b9e324004d36e9342fb5d91f605d
Paddy Jiang <paddy.jiang@maxity.io>
2022-08-29 15:42:17 +0800

    feat: init project

---
+++ contracts/NFTMarketplace.sol
@@ -0,0 +1,10 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/PausableUpgradeable.sol";
+import "@openzeppelin/contracts-upgradeable/security/ReentrancyGuardUpgradeable.sol";
+
+contract NFTMarketPlace is OwnableUpgradeable, PausableUpgradeable, ReentrancyGuardUpgradeable {
+
+}
